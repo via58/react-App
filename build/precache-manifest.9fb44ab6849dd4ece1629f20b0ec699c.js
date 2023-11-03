6d162516db960591b0e531cd03407b1502846c1d
CodeSandbox <support@codesandbox.io>
2023-11-03 07:16:19 +0000

    Initial commit

---
+++ build/precache-manifest.9fb44ab6849dd4ece1629f20b0ec699c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0acc9ba95f0d4287383a42925aeaef0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f59496759f3dcaddd7ef",
+    "url": "/static/js/2.8a4cd978.chunk.js"
+  },
+  {
+    "revision": "b9d5f7f56aa5721bd3e0e6f6b9d79ac8",
+    "url": "/static/js/2.8a4cd978.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7d7b7e0a330edbc4fd08",
+    "url": "/static/js/main.147743ed.chunk.js"
+  },
+  {
+    "revision": "8f4b93024101dfb2a68d",
+    "url": "/static/js/runtime-main.95b35876.js"
+  }
+]);
